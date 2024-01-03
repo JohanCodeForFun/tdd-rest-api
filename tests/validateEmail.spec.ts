97a6b7b717cd690d5d8b00e64027b0b560134ee5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:56:52 +0100

    Add validateEmail tests

--- tests/validateEmail.spec.ts
+++ tests/validateEmail.spec.ts
@@ -1,3 +1,27 @@
-describe('test validate email', () => { 
-  it.todo('test for valid email')
+describe('validate email tests', () => { 
+  it("test for valid email", () => {
+    const email = "anna.andersson@gmail.com";
+
+    const result = validateEmail(email);
+
+    expect(result).toBe(true);
+  });
+
+  it("test for invalid email", () => {
+    const emailA = "";
+    const emailB = "anna.andersson";
+    const emailC = "anna.andersson@gmail";
+    const emailD = "@gmail.com";
+
+    const resultA = validateEmail(emailA);
+    const resultB = validateEmail(emailB);
+    const resultC = validateEmail(emailC);
+    const resultD = validateEmail(emailD);
+
+    expect(resultA).toBe(false);
+    expect(resultB).toBe(false);
+    expect(resultC).toBe(false);
+    expect(resultC).toBe(false);
+  });
+  
  })
