c5aa773793006ff84964982520eba2453573912a
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:59:36 +0100

    type function to return a boolean value

--- src/utils/validatePersonalNumber.ts
+++ src/utils/validatePersonalNumber.ts
@@ -1,4 +1,4 @@
-export function validatePersonalNumber(personalNumber: string) {
+export function validatePersonalNumber(personalNumber: string): boolean {
   const regex = /^\d{6}-\d{4}$/;
   return regex.test(personalNumber);
 }
