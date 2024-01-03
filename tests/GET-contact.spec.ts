e298b7b71fdb5ce6d7ef0e3fc4bc3f72f2316929
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:26:10 +0100

    setup contact test specs

---
+++ tests/GET-contact.spec.ts
@@ -0,0 +1,5 @@
+
+describe('test POST contact', () => { 
+  it.todo('should return 400 on invalid post')
+  it.todo('should return 201 on valid post')
+ })
---
+++ tests/POST-contact.spec.ts
@@ -0,0 +1,15 @@
+const post = {
+  firstname: "Anna",
+  lastname: "Andersson",
+  email: "anna.andersson@gmail.com",
+  personalnumber: "550713-1405",
+  address: "Utvecklargatan 12",
+  zipCode: "111 22",
+  city: "Stockholm",
+  country: "Sweden",
+};
+
+describe('test POST contact', () => { 
+  it.todo('should return 400 on invalid post')
+  it.todo('should return 201 on valid post')
+ })
