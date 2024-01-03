bea2e246de1dd33deb973817d73c908839c0925b
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:49:58 +0100

    Add error handling for invalid POST requests

--- tests/POST-contact.spec.ts
+++ tests/POST-contact.spec.ts
@@ -9,7 +9,21 @@ const post = {
   country: "Sweden",
 };
 
+const error = [
+  {
+    "error": "firstname is missing"
+  },
+  {
+    "error": "email is not valid"
+  }
+]
+
 describe('test POST contact', () => { 
-  it.todo('should return 400 on invalid post')
-  it.todo('should return 201 on valid post')
+  it('should return 400 on invalid post', () => {
+
+  })
+
+  it('should return 201 on valid post', () => {
+    
+  })
  })
