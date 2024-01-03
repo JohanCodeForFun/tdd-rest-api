7814b7f6d695af6a0d992af3759fd1cacc3db0f0
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 21:25:14 +0100

    Add tests for validating city names

---
+++ tests/validateCity.spec.ts
@@ -0,0 +1,26 @@
+
+describe("validate city", () => {
+
+  it("valid city", () => {
+    const city = "Stockholm";
+
+    const result = validateCity(city);
+
+    expect(result).toBe(true);
+  });
+
+  it("invalid city", () => {
+    const cityA = "";
+    const cityB = "err53eedfdf%%#%";
+    const cityC = "3455 45544 44343"
+
+    const resultA = validateCity(cityA);
+    const resultB = validateCity(cityB);
+    const resultC = validateCity(cityC);
+
+    expect(resultA).toBe(false);
+    expect(resultB).toBe(false);
+    expect(resultC).toBe(false);
+  });
+  
+});
