4d179ad10aa407600d223ac7747bfdb33bee4c3d
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:49:22 +0100

    update tests for validating personal numbers

--- tests/validatePersonalNumber.spec.ts
+++ tests/validatePersonalNumber.spec.ts
@@ -1,22 +1,27 @@
+import { validatePersonalNumber } from "../src/utils/validatePersonalNumber";
+
 describe("test validate personal number", () => {
+
   it("test for valid personal number", () => {
-    const personalnumberA = "550713-1405";
+    const personalNumber = "550713-1405";
 
     const result = validatePersonalNumber(personalNumber);
 
     expect(result).toBe(true);
   });
-  it("test for invalid personal number", () => {
-    const personalnumberA = "";
-    const personalnumberB = "553-1405";
-    const personalnumberC = "550713-ABCD";
 
-    const resultA = validatePersonalNumber(personalNumber);
-    const resultB = validatePersonalNumber(personalNumber);
-    const resultC = validatePersonalNumber(personalNumber);
+  it("test for invalid personal number", () => {
+    const personalNumberA = "";
+    const personalNumberB = "553-1405";
+    const personalNumberC = "550713-ABCD";
+
+    const resultA = validatePersonalNumber(personalNumberA);
+    const resultB = validatePersonalNumber(personalNumberB);
+    const resultC = validatePersonalNumber(personalNumberC);
 
     expect(resultA).toBe(false);
     expect(resultB).toBe(false);
     expect(resultC).toBe(false);
   });
+  
 });
