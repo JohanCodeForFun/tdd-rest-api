a936fa10f6da5556d109cc4bae5304204686c84a
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 19:41:19 +0100

    Add tests for validatePersonalNumber

--- tests/validatePersonalNumber.spec.ts
+++ tests/validatePersonalNumber.spec.ts
@@ -1,3 +1,22 @@
-describe('test validate personal number', () => { 
-  it.todo('test for valid personal number')
- })
+describe("test validate personal number", () => {
+  it("test for valid personal number", () => {
+    const personalnumberA = "550713-1405";
+
+    const result = validatePersonalNumber(personalNumber);
+
+    expect(result).toBe(true);
+  });
+  it("test for invalid personal number", () => {
+    const personalnumberA = "";
+    const personalnumberB = "553-1405";
+    const personalnumberC = "550713-ABCD";
+
+    const resultA = validatePersonalNumber(personalNumber);
+    const resultB = validatePersonalNumber(personalNumber);
+    const resultC = validatePersonalNumber(personalNumber);
+
+    expect(resultA).toBe(false);
+    expect(resultB).toBe(false);
+    expect(resultC).toBe(false);
+  });
+});
