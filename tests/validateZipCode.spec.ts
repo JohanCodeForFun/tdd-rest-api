10ae94f993587c35338f1bec0cb0ae6bb763b3f4
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-03 20:48:53 +0100

    create validateZipCode tests

--- tests/validateZipCode.spec.ts
+++ tests/validateZipCode.spec.ts
@@ -1,3 +1,25 @@
-describe('test validate zip', () => { 
-  it.todo('test for valid zip')
- })
+describe("test validate zip code", () => {
+
+  it("test for valid zip code", () => {
+    const zipCode = "111 22";
+
+    const result = validateZipCode(zipCode);
+
+    expect(result).toBe(true);
+  });
+
+  it("test for invalid zip code", () => {
+    const zipCodeA = "111";
+    const zipCodeB = "111 333";
+    const zipCodeC = "ABC DE";
+
+    const resultA = validateZipCode(zipCodeA);
+    const resultB = validateZipCode(zipCodeB);
+    const resultC = validateZipCode(zipCodeC);
+
+    expect(resultA).toBe(false);
+    expect(resultB).toBe(false);
+    expect(resultC).toBe(false);
+  });
+  
+});
