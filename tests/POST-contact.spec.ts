5e2cb6c909a538e361fd80319765c0fd611c7cbb
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-06 20:10:54 +0100

    Refactor POST contact tests

--- tests/POST-contact.spec.ts
+++ tests/POST-contact.spec.ts
@@ -1,29 +1,88 @@
-const post = {
-  firstname: "Anna",
-  lastname: "Andersson",
-  email: "anna.andersson@gmail.com",
-  personalnumber: "550713-1405",
-  address: "Utvecklargatan 12",
-  zipCode: "111 22",
-  city: "Stockholm",
-  country: "Sweden",
-};
+import request from "supertest";
+import app from "../src/app";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer: MongoMemoryServer;
 
 const error = [
   {
-    "error": "firstname is missing"
+    error: "firstname is missing",
   },
   {
-    "error": "email is not valid"
-  }
-]
+    error: "email is not valid",
+  },
+];
 
-describe('test POST contact', () => { 
-  it('should return 400 on invalid post', () => {
+beforeAll(async () => {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
 
-  })
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
-  it('should return 201 on valid post', () => {
-    
-  })
- })
+describe("POST contact", () => {
+  it("should return 400 on empty contact", async () => {
+    const res = await request(app).post("/contact").send({});
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual(error);
+  });
+
+  it("should return error message on missing first name", async () => {
+    const missingFirstName = {
+      lastname: "Andersson",
+      email: "anna.andersson@gmail.com",
+      personalnumber: "550713-1405",
+      address: "Utvecklargatan 12",
+      zipCode: "111 22",
+      city: "Stockholm",
+      country: "Sweden",
+    };
+
+    const res = await request(app).post("/contact").send(missingFirstName);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual(error);
+  });
+
+  it("should return error message on invalid email", async () => {
+    const invalidEmailContact = {
+      firstname: "Anna",
+      lastname: "Andersson",
+      email: "anna.andersson",
+      personalnumber: "550713-1405",
+      address: "Utvecklargatan 12",
+      zipCode: "111 22",
+      city: "Stockholm",
+      country: "Sweden",
+    };
+
+    const res = await request(app).post("/contact").send(invalidEmailContact);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual(error);
+  });
+
+  it("should return 201 on valid post", async () => {
+    const validContact = {
+      firstname: "Anna",
+      lastname: "Andersson",
+      email: "anna.andersson@gmail.com",
+      personalnumber: "550713-1405",
+      address: "Utvecklargatan 12",
+      zipCode: "111 22",
+      city: "Stockholm",
+      country: "Sweden",
+    };
+
+    const res = await request(app).post("/contact").send(validContact);
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("_id");
+  });
+});
