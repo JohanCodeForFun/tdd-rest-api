967a453a7511f9995deacdad117d69a1634a8e4f
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-06 20:39:05 +0100

    Add GET contact tests

--- tests/GET-contact.spec.ts
+++ tests/GET-contact.spec.ts
@@ -1,5 +1,26 @@
+describe("GET contact", () => {
+  it("should return 400 on invalid get", async () => {
+    const res = await request(app).get("/contact/invalid-id");
+    expect(res.statusCode).toEqual(400);
+  });
 
-describe('test POST contact', () => { 
-  it.todo('should return 400 on invalid post')
-  it.todo('should return 201 on valid post')
- })
+  it("should return 200 on valid get", async () => {
+    const getRes = await request(app).get(`/contact/${postRes.body._id}`);
+
+    expect(getRes.body).toEqual(newContact);
+    expect(getRes.statusCode).toEqual(200);
+  });
+
+  it("should return all contacts", async () => {
+    const res = await request(app).get("/contact");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toBeInstanceOf(Array);
+  });
+
+  it("should return contact with id", async () => {
+    const getRes = await request(app).get(`/contact/${postRes.body._id}`);
+
+    expect(getRes.statusCode).toEqual(200);
+    expect(getRes.body).toEqual(newContact);
+  });
+});
