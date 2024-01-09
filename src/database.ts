93355d45dacb251722f02bc3a5d38d87c3d242b3
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-09 14:22:32 +0100

    Add database module for contacts

---
+++ src/database.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+export type Contact = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  personalnumber: string;
+  address: string;
+  zipCode: string;
+  city: string;
+  country: string;
+};
+
+const contactSchema = new mongoose.Schema<Contact>({
+  firstname: String,
+  lastname: String,
+  email: String,
+  personalnumber: String,
+  address: String,
+  zipCode: String,
+  city: String,
+  country: String,
+});
+
+const contactModel = mongoose.model("contact", contactSchema);
+
+export const createContact = async (contactData: Contact) => {
+  return await new contactModel(contactData).save();
+};
