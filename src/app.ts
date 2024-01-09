3838e5231e569902f65c2eefd850e346a5d2265a
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-09 14:23:11 +0100

    Refactor makeApp function to accept createContact as a parameter

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,13 @@ import { validateCountry } from "./utils/validateCountry";
 import { validateName } from "./utils/validateName";
 import { validateZipCode } from "./utils/validateZipCode";
 import { validatePersonalNumber } from "./utils/validatePersonalNumber";
+import { Contact, createContact } from "./database";
 
-export const makeApp = () => {
+type AppPropps = {
+  createContact: (contactData: Contact) => Contact;
+};
+
+export const makeApp = ({ createContact }: AppPropps) => {
   const app = express();
 
   app.use(json());
@@ -29,29 +34,32 @@ export const makeApp = () => {
     } = req.body;
 
     const errors = [];
-    
+
     if (!req.body) {
       errors.push({ error: "You must provide a complete contact" });
     }
 
     if (!validateName(firstname)) {
-      console.log("Validate firstname:", firstname)
       errors.push({ error: "You must provide a firstname" });
     }
     if (!validateName(lastname)) {
       errors.push({ error: "You must provide a lastname" });
     }
     if (!validateEmail(email)) {
-      errors.push({ error: "You must provide a valid email. Example name@domain.com" });
+      errors.push({
+        error: "You must provide a valid email. Example name@domain.com",
+      });
     }
-    if (!validateAddress(address)) { 
+    if (!validateAddress(address)) {
       errors.push({ error: "You must provide a address" });
     }
     if (!validateZipCode(zipCode)) {
       errors.push({ error: "You must provide a zipCode" });
     }
     if (!validatePersonalNumber(personalnumber)) {
-      errors.push({ error: "You must provide a valid personal number. Example, 550713-1405" });
+      errors.push({
+        error: "You must provide a valid personal number. Example, 550713-1405",
+      });
     }
     if (!validateCity(city)) {
       errors.push({ error: "You must provide a city" });
@@ -62,18 +70,17 @@ export const makeApp = () => {
     if (errors.length) {
       res.status(400).json(errors);
     } else {
-      res.status(201).json({ message: "contact created" });
-      // try {
-      //   const contact = req.body;
-      //   // const contact = new Contact(req.body);
-      //   // await contact.save();
-      //   res.status(201).json(contact);
-      // } catch (error) {
-      //   res
-      //     .status(500)
-      //     .json({ error: "An error occurred while saving the contact" });
-      // }
+      try {
+        const contact = await createContact(req.body);
+
+        res.status(201).json(contact);
+      } catch (error) {
+        console.log("catch error", error)
+        res
+          .status(500)
+          .json({ error: "An error occurred while saving the contact" });
+      }
     }
   });
-  return app;
+  return app; 
 };
