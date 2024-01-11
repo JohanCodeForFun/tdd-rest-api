b5f095e57830ef5388a9f1e83d061bfbf318c7b2
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-11 11:25:06 +0100

    Fix typo in GeoLocation type definition and add API key to request headers

--- src/geoLocation.ts
+++ src/geoLocation.ts
@@ -1,11 +1,19 @@
+require("dotenv").config();
 import axios from "axios";
 import { GeoCodingLocation } from "./types/GeoLocation";
+const apiKey = process.env.API_KEY;
 
 export const getGeoCodingLocation = async (city: string): Promise<GeoCodingLocation[]> => {
   try {
     const { data } = await axios.get<GeoCodingLocation[]>(
-      `https://api.api-ninjas.com/v1/geocoding?city=${city}`
+      `https://api.api-ninjas.com/v1/geocoding?city=${city}`,
+      {
+        headers: {
+          'X-Api-Key': `${apiKey}`
+        }
+      }
     );
+
     return data;
   } catch (err) {
     console.error(err);
--- src/types/GeoLocation.ts
+++ src/types/GeoLocation.ts
@@ -1,4 +1,4 @@
 export type GeoCodingLocation = {
   latitude: number;
-  longtitude: number;
+  longitude: number;
 }
