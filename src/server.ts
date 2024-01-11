e4c5c804f060df458c68f0cade067ec94e51f48f
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-11 11:26:56 +0100

    Update server.ts to include new database functions and use environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
+require("dotenv").config();
 import { makeApp } from "./app";
-import { createContact } from "./database";
+import { createContact, getContactById, getAllContacts } from "./database";
 import mongoose from "mongoose";
 
 const port = 3000;
-const app = makeApp({ createContact });
+const app = makeApp({
+  createContact,
+  getContactById,
+  getAllContacts,
+});
 
-mongoose.connect("connection_string").then(() => {
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  throw new Error("MONGO_URI is not defined");
+}
+
+mongoose.connect(mongoUri).then(() => {
   app.listen(port, () => {
+    console.log("Connected to database.");
     console.log("Listening on port", port);
   });
 });
