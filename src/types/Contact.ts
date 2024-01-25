aa2b7e8704699a6c25640237a74891bbc8dafb82
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-25 15:06:33 +0100

    Add Contact type definition

---
+++ src/types/Contact.ts
@@ -0,0 +1,10 @@
+export type Contact = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  personalnumber: string;
+  address: string;
+  zipCode: string;
+  city: string;
+  country: string;
+};
