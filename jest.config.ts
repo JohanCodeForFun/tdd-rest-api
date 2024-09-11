1035a5798736a71a63903a7a733a9aa43367c7ec
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-11 21:44:57 +0200

    remove collect test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  "collectCoverage": true,
+  // "collectCoverage": true,
   collectCoverageFrom: [
     './src/**',
     '!**/node_modules/**',
